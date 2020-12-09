ebf46cc729df038b6dc57281e354e09364446322
frankowskimarcin <marcinfrankowski11@gmail.com>
2020-12-09 11:27:40 +0100

    crud

--- src/main/java/com/example/demo/controller/CarController.java
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Car;
 import com.example.demo.repository.CarRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CarController {
@@ -18,4 +20,36 @@ public class CarController {
     public List<Car> listAllCars(){
         return carRepository.findAll();
     }
+
+    @GetMapping("/cars/{id}")
+    public Car getCar(@PathVariable int id) throws NotFoundException {
+        Optional<Car> car = carRepository.findById(id);
+        if (car.isEmpty()) throw new NotFoundException("id: " + id + " not found");
+        return car.get();
+    }
+
+    @DeleteMapping("/cars/{id}")
+    public void deleteCar(@PathVariable int id) {
+        carRepository.deleteById(id);
+    }
+
+    @PostMapping("/cars")
+    public void addCar(@RequestBody Car car){
+        carRepository.save(car);
+    }
+
+    @PutMapping("/cars/{id}")
+    public void updateCar(@RequestBody Car car, @PathVariable Integer id) throws NotFoundException {
+        Optional<Car> myCar = carRepository.findById(id);
+        if (myCar.isEmpty()) throw new NotFoundException("id: " + id + " not found");
+        car.setId(id);
+        carRepository.save(car);
+    }
+
+//    @PutMapping("/cars/rent/{id}")
+//    public void rentCar(@PathVariable Integer id) throws NotFoundException {
+//        Optional<Car> myCar = carRepository.findById(id);
+//        if (myCar.isEmpty()) throw new NotFoundException("id: " + id + " not found");
+//        myCar.
+//    }
 }
--- src/main/java/com/example/demo/model/Car.java
+++ src/main/java/com/example/demo/model/Car.java
@@ -1,6 +1,5 @@
 package com.example.demo.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -20,5 +19,53 @@ public class Car {
     private boolean rentStatus;
 
     public Car() {
+    }
+
+    public Car(Integer id, String brand, String model, String registrationPlate, boolean rentStatus) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.registrationPlate = registrationPlate;
+        this.rentStatus = rentStatus;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getRegistrationPlate() {
+        return registrationPlate;
+    }
+
+    public void setRegistrationPlate(String registrationPlate) {
+        this.registrationPlate = registrationPlate;
+    }
+
+    public boolean isRentStatus() {
+        return rentStatus;
+    }
+
+    public void setRentStatus(boolean rentStatus) {
+        this.rentStatus = rentStatus;
     }
 }
